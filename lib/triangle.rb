c9ec0c592a430e68d1c419e7ee76be26bead3848
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-02-27 16:31:21 -0800

    commit tree 955f560be2f532f46c7639e5947588366589175a

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
+require 'pry'
 class Triangle
   # write code here
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    case [@side1, @side2, @side3].uniq.size
+    when 1 then pp :equilateral
+    when 2 then pp :isosceles
+    when 3 then  pp :scalene
+    else 
+      begin
+        raise TriangleError
+        
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+    puts "error friend!!"
+  end
 end
+# binding.pry
